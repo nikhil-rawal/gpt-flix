e142dd0606508bff0729bdfbd832c3078a699ce9
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2024-11-10 01:27:09 -0500

    working on form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -10,22 +10,31 @@ const SignIn = () => {
   };
 
   return (
-    <div className="bg-black/50 text-white flex flex-col justify-center items-center p-8">
-      <h1 className="text-white text-4xl text-left">Sign In</h1>
-      <br />
-      <input placeholder="Email or mobile number" className="w-8/12" />
-      <br />
+    <div className="bg-black/60 text-white flex flex-col justify-center items-center p-4 rounded-md">
+      <h1 className="text-white text-4xl text-left my-4 font-semibold w-8/12">
+        Sign In
+      </h1>
 
-      <input placeholder="Password" className="w-8/12" />
-      <br />
+      <input
+        placeholder="Email or mobile number"
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+      />
 
-      <button onClick={handleSignIn} className="bg-[#d22f27] text-white">
+      <input
+        type="password"
+        placeholder="Password"
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+      />
+
+      <button
+        onClick={handleSignIn}
+        className="bg-[#d22f27] text-white my-4 w-8/12 h-10 rounded-md text-xl"
+      >
         Sign In
       </button>
-      <br />
 
-      <p>
-        {/* Don't have an account? <Link to="/signup">Sign Up here!</Link> */}
+      <p className="my-4">
+        Don't have an account? <strong>Sign Up here!</strong>
       </p>
     </div>
   );
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -9,16 +9,38 @@ const SignUp = () => {
     navigate("/signin");
   };
   return (
-    <div className=" bg-black/50 flex flex-col">
-      <h1>Sign Up</h1>
-      <input placeholder="First name" />
-      <input placeholder="Last name" />
-      <input placeholder="Email" />
-      <input placeholder="Mobile number" />
-      <input placeholder="Choose a password" />
-      <button onClick={handleSignUp}>Sign Up</button>
+    <div className=" bg-black/60 text-white flex flex-col justify-center items-center p-4 rounded-md">
+      <h1 className="text-white text-4xl text-left my-4 font-semibold w-8/12">
+        Sign Up
+      </h1>
+      <input
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+        placeholder="First name"
+      />
+      <input
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+        placeholder="Last name"
+      />
+      <input
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+        placeholder="Email"
+      />
+      <input
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+        placeholder="Mobile number"
+      />
+      <input
+        className="w-8/12 h-14 rounded-md bg-black/10 border-[#60605f] border px-3 my-4 focus:border-white focus:border-2 outline-none focus:placeholder:text-xs focus:placeholder:pl-1 placeholder:text-lg"
+        placeholder="Choose a password"
+      />
+      <button
+        onClick={handleSignUp}
+        className="bg-[#d22f27] text-white my-4 w-8/12 h-10 rounded-md text-xl"
+      >
+        Sign Up
+      </button>
       <p>
-        Already have an account? <Link to="/signin">Sign In here!</Link>
+        Already have an account? <strong>Sign In here!</strong>
       </p>
     </div>
   );
