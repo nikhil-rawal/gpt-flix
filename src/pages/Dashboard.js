676925f88998e9f5b8fa73a098f379a005d27d71
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2024-11-17 22:09:51 -0500

    firebase api - to update left

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 const Dashboard = () => {
-  return <div>Welcome. You are successfully signed-in.</div>;
+  const dispatch = useDispatch();
+  const navigateTo = useNavigate();
+  const auth = getAuth();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // dispatch(removeUser())
+        navigateTo("/");
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+  return (
+    <div>
+      <h1>Welcome. You are successfully signed-in.</h1>
+      <br />
+      <button onClick={handleSignOut}>Logout</button>
+    </div>
+  );
 };
 
 export default Dashboard;
