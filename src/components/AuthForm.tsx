3ded52d3103933df33c191b82436f4b07d8ff907
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2025-02-09 12:39:49 -0500

    basic form created - no validations, no signup only signin

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,21 +1,49 @@
+import { RxCrossCircled } from "react-icons/rx";
+
 export default function AuthForm() {
   return (
     // This is for new branch
-    <div className="flex flex-col justify-center rounded-sm  bg-opacity-60 bg-black m-4 p-10">
+    <div className="flex flex-col justify-center rounded-sm  bg-opacity-60 bg-black m-4 p-10 cursor-pointer">
       <header>
-        <h1 className="text-white text-4xl font-bold mb-[28px]">Sign In</h1>
+        <h1 className="text-white text-4xl font-bold mb-[24px]">Sign In</h1>
       </header>
       <form className="flex flex-col items-center justify-center w-full h-full">
-        <input type="text" placeholder="Email" className="m-2 w-80" />
-        <input type="password" placeholder="Password" className="m-2 w-80" />
+        <div className="flex flex-col my-[10px] ">
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-80 h-14 bg-transparent border border-[#606060] py-2 px-4 placeholder:text-[#bababa] rounded-md outline-white text-white"
+          />
+          <text className="text-netflixRed flex items-center text-md my-1">
+            <RxCrossCircled className="font-bold" /> &nbsp;Please enter a valid
+            email
+          </text>
+        </div>
+
+        <div className="flex flex-col my-[10px] ">
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-80 h-14 bg-transparent border border-[#606060] py-2 px-4 placeholder:text-[#bababa] rounded-md outline-white text-white"
+          />
+          <text className="text-netflixRed flex items-center text-md my-1">
+            <RxCrossCircled className="" /> &nbsp;Please enter a valid password
+          </text>
+        </div>
         <button
           type="submit"
-          className="bg-netflixRed text-white rounded-md m-2"
+          className="my-[10px] bg-netflixRed text-white rounded-md m-2 w-80 h-10 font-semibold"
         >
           Sign In
         </button>
       </form>
-      <footer>New here? Signup..</footer>
+      <footer>
+        <p className="my-[10px] text-white text-center">Forgot Password?</p>
+        <p className="mt-[24px] mx-2 text-[#b4b4b4]">
+          New to GPT-Flix?{"  "}
+          <button className="font-semibold text-white">Sign Up Now!</button>
+        </p>
+      </footer>
     </div>
   );
 }
