d928839ef83cfa3324dcfb8991a0026e860d204d
Nikhil Rawal <59576734+nikhil-rawal@users.noreply.github.com>
2025-02-09 22:50:56 -0500

    signup/signin complete css done

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -7,23 +7,25 @@ export default function AuthForm() {
   const [isSignin, setIsSignin] = useState(true);
 
   //fullName, email, password states
-  // const [fullName, setFullName] = useState("");
+  const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-  //fullName, email, password ERROR states
-  // const [fullNameError, setFullNameError] = useState("");
+  //fullName, email, password, confirmPassword ERROR states
+  const [fullNameError, setFullNameError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
 
   //email and password REGEX patterns
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-  //validate fullName, email, password using Regex
-  // const checkFullNameError = (fullName: string) => {
-  //   if (fullName.length < 2) return "Please enter a valid name !";
-  // };
+  //validate fullName, email, password,confirmPassword using Regex
+  const checkFullNameError = (fullName: string) => {
+    return fullName.length < 2 ? "Please enter a valid name !" : "";
+  };
   const checkEmailRegex = (email: string) => {
     if (!emailRegex.test(email)) return "Please enter a valid email !";
     return "";
@@ -32,29 +34,35 @@ export default function AuthForm() {
     if (!passwordRegex.test(password)) return "Please enter a valid password !";
     return "";
   };
+  const checkConfirmPasswordError = (confirmPassword: string) => {
+    if (!confirmPassword) return "Please confirm your password!";
+    if (confirmPassword !== password) return "Passwords do not match !";
+    return "";
+  };
 
   //form submission
   const handleAuthFormSubmit = (e: React.FormEvent) => {
     //prevent default form submission and reset values before submission
     e.preventDefault();
-    // setFullNameError("");
-    setEmailError("");
-    setPasswordError("");
 
     //input validations
-    // const isFullNameValid = checkFullNameError(fullName);
+    const isFullNameValid = checkFullNameError(fullName);
     const isEmailValid = checkEmailRegex(email);
     const isPasswordValid = checkPasswordRegex(password);
+    const isConfirmPasswordValid = checkConfirmPasswordError(confirmPassword);
 
     //if there is an error, set the error message
-    // if (isFullNameValid) {
-    //   setFullNameError(isFullNameValid)
-    // }
+    if (isFullNameValid) {
+      setFullNameError(isFullNameValid);
+    }
     if (isEmailValid) {
       setEmailError(isEmailValid);
     }
     if (isPasswordValid) {
       setPasswordError(isPasswordValid);
+    }
+    if (isConfirmPasswordValid) {
+      setConfirmPasswordError(isConfirmPasswordValid);
     }
 
     //if no error found, console.log(email,password)
@@ -65,6 +73,7 @@ export default function AuthForm() {
 
   return (
     <div className="flex flex-col justify-center rounded-sm  bg-opacity-60 bg-black m-4 p-10">
+      {/* header */}
       <header>
         <h1 className="text-white text-4xl font-bold mb-[24px]">
           {isSignin ? "Sign In" : "Sign Up"}
@@ -74,33 +83,33 @@ export default function AuthForm() {
         className="flex flex-col items-center justify-center w-full h-full"
         onSubmit={handleAuthFormSubmit}
       >
-        {/* {!isSignin && (
-          <div className="flex flex-col mt-[10px] mb-[5px] ">
+        {/* if signUp form, show fullName input */}
+        {!isSignin && (
+          <div className="flex flex-col my-[6px]">
             <input
               type="text"
-              placeholder="First Name"
+              placeholder="Full Name"
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
-              onBlur={() => setFullNameError(isFullNameValid(fullName))}
+              onBlur={() => setFullNameError(checkFullNameError(fullName))}
               className={`w-80 h-14 bg-transparent border-2 py-2 px-4 transition-all ease-in-out focus:placeholder:text-white focus:placeholder:text-[13px] focus:placeholder: rounded-md text-white outline-none ring-0 active:outline-none active:ring-0 border-[#606060] focus:border-white ${
                 fullNameError && "border-[#e50914]"
               }`}
             />
-            <p
-              className={`text-netflixRed flex items-center text-md my-0 max-w-80`}
-            >
-              {fullNameError ? (
-                <>
-                  <RxCrossCircled className="font-bold" /> &nbsp;{fullNameError}{" "}
-                </>
-              ) : (
-                <> &nbsp;</>
-              )}
-            </p>
+            {fullNameError ? (
+              <p
+                className={`text-netflixRed flex items-center text-md my-0 max-w-80 `}
+              >
+                <RxCrossCircled className="font-bold" /> &nbsp;{fullNameError}
+              </p>
+            ) : (
+              <>&nbsp;</>
+            )}
           </div>
-        )} */}
+        )}
 
-        <div className="flex flex-col mt-[10px] mb-[5px] ">
+        {/* email input */}
+        <div className="flex flex-col my-[6px]">
           <input
             type="email"
             placeholder="Email"
@@ -111,22 +120,19 @@ export default function AuthForm() {
               emailError && "border-[#e50914]"
             }`}
           />
-          <p
-            className={`text-netflixRed ${
-              emailError ? "flex" : "hidden"
-            } items-center text-md my-0 max-w-80`}
-          >
-            {emailError ? (
-              <>
-                <RxCrossCircled className="font-bold" /> &nbsp;{emailError}{" "}
-              </>
-            ) : (
-              <> &nbsp;</>
-            )}
-          </p>
+          {emailError ? (
+            <p
+              className={`text-netflixRed flex items-center text-md my-0 max-w-80`}
+            >
+              <RxCrossCircled className="font-bold" /> &nbsp;{emailError}
+            </p>
+          ) : (
+            <>&nbsp;</>
+          )}
         </div>
 
-        <div className="flex flex-col mt-[5px] mb-[10px] ">
+        {/* password input */}
+        <div className="flex flex-col my-[6px] ">
           <input
             type="password"
             placeholder="Password"
@@ -134,37 +140,69 @@ export default function AuthForm() {
             onChange={(e) => setPassword(e.target.value)}
             onBlur={() => setPasswordError(checkPasswordRegex(password))}
             className={`w-80 h-14 bg-transparent border-2 py-2 px-4 placeholder:text-[#bababa] rounded-md text-white outline-none ring-0 active:outline-none active:ring-0 border-[#606060] focus:border-white ${
-              passwordError && "border-[#e50914] focus:border-[#e50914]"
+              passwordError && "border-[#e50914] "
             }`}
           />
-          <p
-            className={`text-netflixRed ${
-              passwordError ? "flex" : "hidden"
-            } items-center text-md my-0 max-w-80`}
-          >
-            {passwordError ? (
-              <>
-                <RxCrossCircled className="font-bold" /> &nbsp;{passwordError}{" "}
-              </>
-            ) : (
-              <> &nbsp;</>
-            )}
-          </p>
+          {passwordError ? (
+            <p
+              className={`text-netflixRed flex items-center text-md my-0 max-w-80`}
+            >
+              <RxCrossCircled className="font-bold" /> &nbsp;{passwordError}
+            </p>
+          ) : (
+            <>&nbsp;</>
+          )}
         </div>
+
+        {/* if signUp form, confirm password input */}
+        {!isSignin && (
+          <div className="flex flex-col my-[6px] ">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              onBlur={() =>
+                setConfirmPasswordError(
+                  checkConfirmPasswordError(confirmPassword)
+                )
+              }
+              className={`w-80 h-14 bg-transparent border-2 py-2 px-4 placeholder:text-[#bababa] rounded-md text-white outline-none ring-0 active:outline-none active:ring-0 border-[#606060] focus:border-white ${
+                confirmPasswordError && "border-[#e50914] "
+              }`}
+            />
+            {confirmPasswordError ? (
+              <p
+                className={`text-netflixRed flex items-center text-md my-0 max-w-80`}
+              >
+                <RxCrossCircled className="font-bold" /> &nbsp;
+                {confirmPasswordError}
+              </p>
+            ) : (
+              <>&nbsp;</>
+            )}
+          </div>
+        )}
+
+        {/* submit button */}
         <button
           type="submit"
-          className="mt-[5px] mb-[10px] bg-netflixRed text-white rounded-md m-2 w-80 h-10 font-semibold"
+          className="mt-[8px] mb-[12px] bg-netflixRed text-white rounded-md m-2 w-80 h-10 font-semibold"
         >
           {isSignin ? "Sign In" : "Sign Up"}
         </button>
       </form>
+
+      {/* footer */}
       <footer>
+        {/* if signIn form, forgot password */}
         {isSignin ? (
           <p className="my-[12px] text-white text-center">
             <button className="text-white">Forgot Password?</button>
           </p>
         ) : null}
 
+        {/* toggle signIn or signUp button - depending on form */}
         <p className="mt-[24px] mx-2 text-[#b4b4b4]">
           {isSignin ? "New to GPT-Flix?" : "Already a GPT-Flix member?"}
           &nbsp;&nbsp;
